6439c1703545010ba4145d5456f0f65bff5a118a
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-28 16:27:13 -0400

    project planned

--- githubusercards/src/App.js
+++ githubusercards/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={};
+  }
+  render(){
+    return(
+      <div>
+      <h1>Happy Birthday!</h1>
+      <p>
+        - [ ] Fetch data from the Github API for a Github user<br/>
+        - [ ] Display the user data on the DOM<br/>
+        - [ ] Use class components when you need to hold any state or use any lifecycle methods</p>
+        </div>
+    )
+  }
 }
 
 export default App;
